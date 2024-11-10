e0896d4902cee28960ceda2aa401880f9110d8a2
Erdis Halluni <erdis.halluni.1@ens.etsmtl.ca>
2024-11-10 14:17:16 -0500

    Maven Springboot dependencies added

---
+++ chessAiAppBE/src/main/java/com/chessBackendAI/chessAiAppBE/ChessAiAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.chessBackendAI.chessAiAppBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessAiAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessAiAppBeApplication.class, args);
+	}
+
+}
---
+++ chessAiAppBE/src/test/java/com/chessBackendAI/chessAiAppBE/ChessAiAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessBackendAI.chessAiAppBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessAiAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
